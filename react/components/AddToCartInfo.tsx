c877a0c885a97ddb017fc461443f0a0bb579831f
daniel17110290 <danielfdz0290@gmail.com>
2022-09-17 17:57:21 -0500

    feat: create add-to-cart-info-component

---
+++ react/components/AddToCartInfo.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useProduct } from "vtex.product-context"
+import { useOrderForm } from "vtex.order-manager/OrderForm"
+import ButtonGroup from "./ButtonGroup";
+import { generateBlockClass } from "@vtex/css-handles";
+import styles from "./styles.css"
+
+
+const AddToCartInfo = ({ blockClass }: { blockClass: string }) => {
+  const container = generateBlockClass(styles.container, blockClass)
+  const container__item = generateBlockClass(styles.container__item, blockClass)
+  const productInfo = useProduct()
+  const { orderForm: {
+    items,
+    totalizers
+  } } = useOrderForm()
+  console.log("este producto tiene esta info:", productInfo)
+  console.log("estos son mis totales:", totalizers[0])
+  return (
+    <div className={container}>
+      {items.map((item: any, index: number) => {
+        console.log(item)
+        return (
+          <div key={index} className={container__item}>
+            <div>
+              <img src={item.imageUrls.at1x} />
+            </div>
+            <div>
+              <p>{item.name}</p>
+              <p>{item.id}</p>
+              <p>${item.price}</p>
+              <p>Cant:{item.quantity}</p>
+            </div>
+          </div>
+        )
+      })}
+      <div>
+        <p>Tenemos {items.length} items en tu compra</p>
+        <p>Total: {totalizers[0].value}</p>
+      </div>
+      <ButtonGroup />
+    </div>
+  )
+}
+
+export default AddToCartInfo
